8c0d122caf809525cb8dea32fb8fe11bcb52aaf1
Alex <116267528+AlexMGarbalyauskas@users.noreply.github.com>
2024-06-13 17:21:58 +0100

    Update loader.jsx

--- src/loader/loader.jsx
+++ src/loader/loader.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactLoading from 'react-loading';
-import './loader.css'; // Make sure to create this CSS file
+import './loader.css'; 
 
 const Loader = () => {
   return (
